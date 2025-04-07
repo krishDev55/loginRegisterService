913904d24e3384ab9394182f9efc92d1437448ee
Actsys 10 <Actsys 10@DESKTOP-Q76TTOQ>
2025-04-07 17:04:28 +0530

    All code  push to  git hub

---
+++ src/test/java/com/login_RegisterService/LoginRegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login_RegisterService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
