2a22377c8fd33ee8e5979891f59b027d92ca8bdc
Actsys 10 <Actsys 10@DESKTOP-Q76TTOQ>
2025-04-07 17:02:52 +0530

    add the Pom.xml File to git gub Also addding the Main   method name

---
+++ src/main/java/com/login_RegisterService/LoginRegisterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.login_RegisterService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegisterServiceApplication.class, args);
+	}
+
+}
